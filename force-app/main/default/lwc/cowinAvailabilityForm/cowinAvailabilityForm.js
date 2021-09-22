22e2af83323531560bb4f989dffea9ec2b46c82e
Koshambee18 <koshambee@gmail.com>
2021-09-22 15:35:30 +0530

    Changed at 03:35

--- force-app/main/default/lwc/cowinAvailabilityForm/cowinAvailabilityForm.js
+++ force-app/main/default/lwc/cowinAvailabilityForm/cowinAvailabilityForm.js
@@ -1,22 +1,62 @@
-import { LightningElement, track, wire} from 'lwc';
+import {
+    LightningElement,
+    track,
+    wire
+} from 'lwc';
 import getDetails from '@salesforce/apex/CowinController.getDetails';
 import insertCowinDetails from '@salesforce/apex/CowinController.insertCowinDetails';
-import { NavigationMixin } from 'lightning/navigation';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import {
+    NavigationMixin
+} from 'lightning/navigation';
+import {
+    ShowToastEvent
+} from 'lightning/platformShowToastEvent';
 
-const columns = [
-    { label: 'CenterId', fieldName: 'centerid'},
-    { label: 'Name', fieldName: 'Name'},
-    { label: 'BlockName', fieldName: 'BlockName'},
-    { label: 'District', fieldName: 'District'},
-    { label: 'Pincode', fieldName: 'Pincode'},
-    { label: 'AvailableCapacityDose2', fieldName: 'AvailableCapacityDose2'},
-    { label: 'Fee', fieldName: 'Fee'},
-    { label: 'MinAgeLimit', fieldName: 'MinAgeLimit'},
-    { label: 'AvailableCapacity', fieldName: 'AvailableCapacity'},
-    { label: 'AvailableCapacityDose1', fieldName: 'AvailableCapacityDose1'},
-    { label: 'Slots', fieldName: 'Slots'}
-    
+const columns = [{
+        label: 'CenterId',
+        fieldName: 'centerid'
+    },
+    {
+        label: 'Name',
+        fieldName: 'Name'
+    },
+    {
+        label: 'BlockName',
+        fieldName: 'BlockName'
+    },
+    {
+        label: 'District',
+        fieldName: 'District'
+    },
+    {
+        label: 'Pincode',
+        fieldName: 'Pincode'
+    },
+    {
+        label: 'AvailableCapacityDose2',
+        fieldName: 'AvailableCapacityDose2'
+    },
+    {
+        label: 'Fee',
+        fieldName: 'Fee'
+    },
+    {
+        label: 'MinAgeLimit',
+        fieldName: 'MinAgeLimit'
+    },
+    {
+        label: 'AvailableCapacity',
+        fieldName: 'AvailableCapacity'
+    },
+    {
+        label: 'AvailableCapacityDose1',
+        fieldName: 'AvailableCapacityDose1'
+    },
+    {
+        label: 'Slots',
+        fieldName: 'Slots'
+    }
+
 ];
 
 
@@ -24,64 +64,98 @@ export default class CowinAvailabilityForm extends LightningElement {
 
     @track cowinData = [];
     @track cowinDetails = [];
-    @track size;
-    @track cardTitle = 'Total Cowin Avilability Today :'
-    
+    @track size = 0;
+    @track cardTitle = '';
+    @track available = false;
 
-    pincode ='';
-    cowinDate ='';
+
+    pincode = '';
+    cowinDate = '';
     columns = columns;
 
-    handlecowin(event){
-        if(event.target.label=='Enter Pincode of the city'){
+    handlecowin(event) {
+        if (event.target.label == 'Enter Pincode of the city') {
             this.pincode = event.target.value;
         }
-        if(event.target.label=='Enter the date of availability'){
+        if (event.target.label == 'Enter the date of availability') {
             this.cowinDate = event.target.value;
-        }  
-    
+        }
+
     }
 
-//To show data in table
-submitForm(event){
+    //To show data in table
+    submitForm(event) {
 
-        getDetails({pincode:this.pincode,cowinDate:this.cowinDate})
-        .then((result) => {
-            console.log('COWINDATA');
-            console.log(JSON.parse(JSON.stringify(result)));
-            this.cowinDetails = JSON.parse(JSON.stringify(result))[0].cowinDetailsList;
-            this.cowinData = JSON.parse(JSON.stringify(result));
-            this.size = this.cowinData.length;
-        })
-        .catch((error) => {
-            this.error = error;
-            console.log(error);
-            this.cowinDataArray = undefined;
-        });
-     }
-        closeModal() {
+        getDetails({
+                pincode: this.pincode,
+                cowinDate: this.cowinDate
+            })
+            .then((result) => {
+                console.log('COWINDATA');
+                console.log(JSON.parse(JSON.stringify(result)));
+                this.cowinDetails = JSON.parse(JSON.stringify(result))[0].cowinDetailsList;
+                this.cowinData = JSON.parse(JSON.stringify(result));
+                this.size = this.cowinData.length;
+                if (this.size > 0) {
+                    this.available = true;
+                    this.cardTitle = 'Total Cowin Avilability Today : '+this.size;
+                }
+            })
+            .catch((error) => {
+                this.error = error;
+                console.log(error);
+                this.cowinDataArray = undefined;
+                        const event = new ShowToastEvent({
+                        title: 'Error',
+                        message: 'No Records found for this date!',
+                        variant: 'Error'
+                    });
+                    this.dispatchEvent(event);
+            });
+    }
+    closeModal() {
         window.location.reload();
-        }
-        
-//To insert cowin availabilty records
-     insertCowinData(event){
-          insertCowinDetails({pincode:this.pincode,cowinDate:this.cowinDate,cowinAvailablityList:this.cowinDetails})
+        //eval("$A.get('e.force:refreshView').fire();");
+
+    }
+
+    //To insert cowin availabilty records
+    insertCowinData(event) {
+        insertCowinDetails({
+                pincode: this.pincode,
+                cowinDate: this.cowinDate,
+                cowinAvailablityList: this.cowinDetails
+            })
 
             .then((result) => {
-                const event = new ShowToastEvent({
-                title: 'Saved',
-                message: 'Records Inserted Successfully',
-                variant: 'success'
+                if (this.size == 0) {
+
+                    const event = new ShowToastEvent({
+                        title: 'Error',
+                        message: 'No Records to sync, Please click Search button to get the Data',
+                        variant: 'Error'
+                    });
+                    this.dispatchEvent(event);
+                } else {
+
+                    const event = new ShowToastEvent({
+
+                        title: 'Saved',
+                        message: 'Records Inserted Successfully',
+                        variant: 'success'
+                    });
+                    this.dispatchEvent(event);
+                }
+
+
+            })
+
+            .catch((error) => {
+                this.error = error;
+                console.log(error);
             });
-        this.dispatchEvent(event);
-
-        })
-        .catch((error) => {
-            this.error = error;
-            console.log(error);
-        });
 
 
 
-     }
+    }
 }
